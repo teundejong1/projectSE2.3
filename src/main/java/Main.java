c51c9daaee9e1c8371cab49f4d2964699f7b12f3
teundejong1 <58029921+teundejong1@users.noreply.github.com>
2021-03-30 10:54:44 +0200

    gestolen TTT

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+package main.java;
+
+import java.util.Scanner;
+
+/**
+ * Created by Philip Rodriguez on 3/14/2016.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        TTT game = new TTT();
+        game.initializeBoard();
+        System.out.println("Tic-Tac-Toe!");
+        do
+        {
+            System.out.println("Current board layout:");
+            game.printBoard();
+            int row;
+            int col;
+            do
+            {
+                System.out.println("Player " + game.getCurrentPlayerMark() + ", enter an empty row and column to place your mark!");
+                row = scan.nextInt()-1;
+                col = scan.nextInt()-1;
+            }
+            while (!game.placeMark(row, col));
+            game.changePlayer();
+        }
+        while(!game.checkForWin() && !game.isBoardFull());
+        if (game.isBoardFull() && !game.checkForWin())
+        {
+            System.out.println("The game was a tie!");
+        }
+        else
+        {
+            System.out.println("Current board layout:");
+            game.printBoard();
+            game.changePlayer();
+            System.out.println(Character.toUpperCase(game.getCurrentPlayerMark()) + " Wins!");
+        }
+    }
+}
