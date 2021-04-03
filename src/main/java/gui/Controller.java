2a59fb73d782556ede592899d9ec3385da0ec139
pueper <tombeugel@gmail.com>
2021-04-03 22:03:40 +0200

    fix
    en een fix zodat de eerdere commit werkt
    (gewoon wat shit uitgecomment)

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -28,7 +28,6 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.concurrent.ThreadPoolExecutor;
 
-import games.ai.AI;
 
 public class Controller {
     @FXML
@@ -49,48 +48,48 @@ public class Controller {
     }
 
 
-    public void TTT(ActionEvent actionEvent) {
-
-        try {
-            // close menu
-            Stage menu = (Stage) TTTvsAI.getScene().getWindow();
-            // do what you have to do
-            menu.close();
-
-            loader = new FXMLLoader(getClass().getResource("/fxml/menu1.fxml"));
-            root = loader.load();
-            controller =  loader.getController();
-
-            // open TTT
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/TTT1.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.setTitle("Tic Tac Toe");
-            stage.setScene(new Scene(root1));
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Let's Play!
-        if(actionEvent.getSource() == TTTvsspeler) {
-
-        } else if(actionEvent.getSource() == TTTvsAI) {
-            // maak speler
-            Player player = new Player('x', "[PH] - FRANKENSTEIN");
-            AI ai = new AI('o', "[PH] - AI");
-            // start a game
-            Maintest.ticTacToe = new Maintest(player, ai);
-            System.out.println(Maintest.ticTacToe.ticTacToeManager.getCurrentPlayer().getPlayerName());
-            try {
-                Maintest.ticTacToe.ticTacToeManager.start(player, ai);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    public void TTT(ActionEvent actionEvent) {
+//
+//        try {
+//            // close menu
+//            Stage menu = (Stage) TTTvsAI.getScene().getWindow();
+//            // do what you have to do
+//            menu.close();
+//
+//            loader = new FXMLLoader(getClass().getResource("/fxml/menu1.fxml"));
+//            root = loader.load();
+//            controller =  loader.getController();
+//
+//            // open TTT
+//            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/TTT1.fxml"));
+//            Parent root1 = (Parent) fxmlLoader.load();
+//            Stage stage = new Stage();
+//            stage.initModality(Modality.APPLICATION_MODAL);
+//            stage.initStyle(StageStyle.UNDECORATED);
+//            stage.setTitle("Tic Tac Toe");
+//            stage.setScene(new Scene(root1));
+//            stage.show();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // Let's Play!
+//        if(actionEvent.getSource() == TTTvsspeler) {
+//
+//        } else if(actionEvent.getSource() == TTTvsAI) {
+//            // maak speler
+//            Player player = new Player('x', "[PH] - FRANKENSTEIN");
+//            AI ai = new AI('o', "[PH] - AI");
+//            // start a game
+//            Maintest.ticTacToe = new Maintest(player, ai);
+//            System.out.println(Maintest.ticTacToe.ticTacToeManager.getCurrentPlayer().getPlayerName());
+//            try {
+//                Maintest.ticTacToe.ticTacToeManager.start(player, ai);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
     public void playTicTacToe(ActionEvent actionEvent) {
         //TODO
@@ -105,42 +104,42 @@ public class Controller {
         //TODO
     }
 
-    /*
-    * Do moves --> actie om een zet uit te voeren
-    */
-    public void setGrid(ActionEvent event) {
-
-        if(event.getSource() == butt00) {
-
-            butt00.setText( "" + Maintest.ticTacToe.ticTacToeManager.getCurrentPlayerMark()); //hackerman
-            Maintest.ticTacToe.ticTacToeManager.checkMove(0, 0);
-            Maintest.ticTacToe.ticTacToeManager.placeMove(0, 0);
-            Maintest.ticTacToe.ticTacToeManager.printBoard(Maintest.ticTacToe.ticTacToeManager.getBoard()); //print
-            Maintest.ticTacToe.ticTacToeManager.changePlayer();
-//            System.out.println("check");
-
-            /*if(GameManager.checkMove(0, 1)){
-                 TicTacToeManager.makeMove(0,1);
-                  butt00.setText(currentPlayer.getPlayerMark());
-             }*/
-        } else if(event.getSource() == butt01) {
-
-        } else if(event.getSource() == butt02) {
-
-        } else if(event.getSource() == butt10) {
-
-        } else if(event.getSource() == butt11) {
-
-        } else if(event.getSource() == butt12) {
-
-        } else if(event.getSource() == butt20) {
-
-        } else if(event.getSource() == butt21) {
-
-        } else if(event.getSource() == butt22) {
-
-        }
-    }
+//    /*
+//    * Do moves --> actie om een zet uit te voeren
+//    */
+//    public void setGrid(ActionEvent event) {
+//
+//        if(event.getSource() == butt00) {
+//
+//            butt00.setText( "" + Maintest.ticTacToe.ticTacToeManager.getCurrentPlayerMark()); //hackerman
+//            Maintest.ticTacToe.ticTacToeManager.checkMove(0, 0);
+//            Maintest.ticTacToe.ticTacToeManager.placeMove(0, 0);
+//            Maintest.ticTacToe.ticTacToeManager.printBoard(Maintest.ticTacToe.ticTacToeManager.getBoard()); //print
+//            Maintest.ticTacToe.ticTacToeManager.changePlayer();
+////            System.out.println("check");
+//
+//            /*if(GameManager.checkMove(0, 1)){
+//                 TicTacToeManager.makeMove(0,1);
+//                  butt00.setText(currentPlayer.getPlayerMark());
+//             }*/
+//        } else if(event.getSource() == butt01) {
+//
+//        } else if(event.getSource() == butt02) {
+//
+//        } else if(event.getSource() == butt10) {
+//
+//        } else if(event.getSource() == butt11) {
+//
+//        } else if(event.getSource() == butt12) {
+//
+//        } else if(event.getSource() == butt20) {
+//
+//        } else if(event.getSource() == butt21) {
+//
+//        } else if(event.getSource() == butt22) {
+//
+//        }
+//    }
 
 
     public void howToTTT(ActionEvent actionEvent) {
