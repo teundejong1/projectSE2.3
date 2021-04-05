f4ade5fcda1b33f78e15e5b75b731f71f376eef4
teundejong1 <58029921+teundejong1@users.noreply.github.com>
2021-04-05 16:32:25 +0200

    semi- working othello

--- src/main/java/cli/Main.java
+++ src/main/java/cli/Main.java
@@ -1,18 +1,24 @@
 package cli;
 
-import games.Game;
-import games.GameEnum;
-import games.TicTacToe;
+import games.*;
+import games.board.Mark;
+import games.board.SetOutOfBoundsException;
 import player.Player;
 import player.PlayerFactory;
 import player.PlayerType;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SetOutOfBoundsException {
         Player p1 = PlayerFactory.createCLIPlayer("Teun");
-        Player p2 = PlayerFactory.createAIPlayer("Esther", GameEnum.TTT);
+        Player p2 = PlayerFactory.createCLIPlayer("Esther");
 
-        Game game = new TicTacToe(PlayerType.ONE);
+        //Game game = new TicTacToe(PlayerType.ONE);
+        //game.start(p1, p2);
+
+        //Game game = new TicTacToe(PlayerType.ONE);
+        Game game = new Othello(PlayerType.ONE);
         game.start(p1, p2);
+
+
     }
 }
