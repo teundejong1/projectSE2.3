0077ba4ade0e9053a7b1562e025ee31a7d2288ee
Eva <jakobseva@gmail.com>
2021-04-13 13:59:45 +0200

    poging 2.1 voor oefentoernooi

--- src/main/java/networking/eventhandlers/MatchHandler.java
+++ src/main/java/networking/eventhandlers/MatchHandler.java
@@ -13,7 +13,50 @@ public class MatchHandler implements Handler {
     public void handle(String response) {
         manager.setState(new PlayingState());
         Map<String, String> map = Parser.parseMap(response);
+        MatchHash match = new MatchHash(response);
         // doe iets met de map
+        // {GAMTYPE: "<speltype>", PLAYERTOMOVE: "<naam speler1>", OPPONENT: "<naam tegenstander>"}
+        View.startOnlineMatch(match.getGame());
     }
 
+    /**
+     * Inner class to handle the Hashmap
+     */
+    class MatchHash{
+        // {GAMTYPE: "<speltype>", PLAYERTOMOVE: "<naam speler1>", OPPONENT: "<naam tegenstander>"}
+        private String speltype, playerToMove, opponent;
+
+        public MatchHash(String details){
+            new MatchHash(Parser.parseMap(details));
+        }
+
+        public MatchHash(Map<String, String> map){
+            speltype = map.get("GAMETYPE");
+            playerToMove = map.get("PLAYERTOMOVE");
+            opponent = map.get("OPPONENT");
+        }
+
+        public String getSpeltype(){
+            return speltype;
+        }
+
+        public GameEnum getGame(){
+            switch(speltype){
+                case "Tic-tac-toe":
+                    return GameEnum.TTT;
+                case "Othello":
+                case "Reversi":
+                    return GameEnum.OTHELLO;
+                default: return null;
+            }
+        }
+
+        public String getOpponent(){
+            return opponent;
+        }
+
+        public String getPlayerToMove(){
+            return playerToMove;
+        }
+    }
 }
